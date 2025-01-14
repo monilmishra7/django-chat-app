dae561700c587363b521abb930fec1fce4cb7d53
monilmishra7 <monilmishra4@gmail.com>
2025-01-14 16:28:05 +0530

    feat/ui for the chat-app and setup of realtime communication

---
+++ chat/consumers.py
@@ -0,0 +1,57 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import ChatRoom, Message
+from django.contrib.auth.models import User
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_id = self.scope['url_route']['kwargs']['room_id']
+        self.room_group_name = f'chat_{self.room_id}'
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        
+        # Save message to database
+        await self.save_message(message)
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'sender': self.scope['user'].username
+            }
+        )
+
+    async def chat_message(self, event):
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': event['message'],
+            'sender': event['sender']
+        }))
+
+    @database_sync_to_async
+    def save_message(self, message):
+        room = ChatRoom.objects.get(id=self.room_id)
+        Message.objects.create(
+            room=room,
+            sender=self.scope['user'],
+            content=message
+        )
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import ChatRoom, Message
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def index(request):
+    users = User.objects.exclude(id=request.user.id)
+    return render(request, 'chat/index.html', {'users': users})
+
+@login_required
+def room(request, user_id):
+    chat_user = User.objects.get(id=user_id)
+    users = User.objects.exclude(id=request.user.id)
+    
+    # Get or create chat room
+    room = ChatRoom.objects.filter(participants=request.user)\
+                          .filter(participants=chat_user)\
+                          .first()
+                          
+    if not room:
+        room = ChatRoom.objects.create()
+        room.participants.add(request.user, chat_user)
+    
+    # Get messages
+    messages = Message.objects.filter(room=room).order_by('timestamp')
+    
+    return render(request, 'chat/room.html', {
+        'chat_user': chat_user,
+        'users': users,
+        'room': room,
+        'messages': messages
+    })
